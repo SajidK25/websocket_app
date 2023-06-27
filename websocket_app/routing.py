b491f3b3bd6ff1963494eed619553bdb6310a525
Sajid25 <sajidk25@gmail.com>
2023-06-27 22:37:02 +0600

    modified:   docker-compose.override.yml 	modified:   websocket_app/routing.py

--- websocket_app/routing.py
+++ websocket_app/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from color_app.consumers import ColorConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/color/$', ColorConsumer.as_asgi()),
+    re_path(r'wss/color/$', ColorConsumer.as_asgi()),
 ]
